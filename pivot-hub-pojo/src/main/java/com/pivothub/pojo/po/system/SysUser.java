f24bd10149adbf9102c6f7371b75ac0e82024868
lhb <2226338422@qq.com>
2026-09-23 00:07:33 +0800

    feat:user实体对象属性修改

--- pivot-hub-pojo/src/main/java/com/pivothub/pojo/po/system/SysUser.java
+++ pivot-hub-pojo/src/main/java/com/pivothub/pojo/po/system/SysUser.java
@@ -20,17 +20,10 @@ public class SysUser extends BasePo {
     /** 用户编码 */
     private String code;
 
-    /** 用户名 */
-    private String username;
 
     /** 昵称 */
     private String nickname;
 
-    /** 密码（AES加密） */
-    private String password;
-
-    /** 手机号（AES加密） */
-    private String phone;
 
     /** 邮箱 */
     private String email;
