d5199c85be9e5dea04da46b0268f97ca72a533da
lhb <2226338422@qq.com>
2026-09-26 12:05:39 +0800

    feat:system模块代码清除

--- pivot-hub-system/src/main/java/com/pivothub/system/service/SysRoleService.java
+++
@@ -1,7 +0,0 @@
-package com.pivothub.system.service;
-
-import com.baomidou.mybatisplus.extension.service.IService;
-import com.pivothub.pojo.po.SysRole;
-
-public interface SysRoleService extends IService<SysRole> {
-}
--- pivot-hub-system/src/main/java/com/pivothub/system/service/SysUserService.java
+++
@@ -1,7 +0,0 @@
-package com.pivothub.system.service;
-
-import com.baomidou.mybatisplus.extension.service.IService;
-import com.pivothub.pojo.po.SysUser;
-
-public interface SysUserService extends IService<SysUser> {
-}
